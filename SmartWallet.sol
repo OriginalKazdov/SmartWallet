5eb74406250c41580d3799440b8d594d6ec0fc48
Juan Cruz Dovzak <juandovzak@gmail.com>
2025-12-02 16:23:38 -0300

    Implement of the implementation

--- SmartWallet.sol
+++ SmartWallet.sol
@@ -3,28 +3,31 @@ pragma solidity >=0.8.18;
 
 contract SmartWallet{
     address public owner;
-    //necesito una variable amount?
-    //mapping de users? esto creo que si
-    event Withdraw(address user, uint256 amount);
+    mapping(address => uint256) user;
+    event Deposited(address user, uint256 amount);
+    event Withdrawn(address user, uint256 amount);
+
+    modifier OnlyOwner(){
+      owner = msg.sender;
+      _;
+    }
 
     constructor(){
       owner = msg.sender;
     }
 
-    //deberia dejar depositar a los usuarios, posiblemente cantidades
-    //especificas si es posible. pero creo que eso se puede hacer con
-    //msg.value
     function deposit() external payable{
-
+      require(msg.value > 0, "Deposit must be greater than 0");
+      user[msg.sender] += msg.value;
+      emit Deposited(msg.sender, msg.value);
     }
 
-    //deberia dejar al owner retirar cantidades exactas del fondo
-    //SOLO el OWNER
-    function withdraw() external{
+    function withdraw(uint256 _amount) external OnlyOwner{
+      require(_amount > 0, "You are not the owner");
 
     }
 
     function getBalance() external view returns(uint){
-        return address(this).balance;
+      return address(this).balance;
     }
 }
